8cf9d0d9eb64e0176b533e88412846fa29fd40b3
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-21 15:30:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace TaskJunior2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameUser;
+            string middleLine;
+            string frame = "";
+
+            char symbolInput;
+
+            int someString;
+
+            Console.WriteLine("Добро пожаловать, в обработку в красивой форме вашего Имени.");
+
+            Console.Write("Введите ваше замечательное Имя: ");
+            nameUser = Console.ReadLine();
+
+            Console.Write("Введите символ: ");
+            symbolInput = Convert.ToChar(Console.ReadLine());
+
+            someString = nameUser.Length;
+            middleLine = symbolInput + nameUser + symbolInput;
+            frame = symbolInput + frame + symbolInput;
+
+            for (int i = 0; i < someString; i++)
+            {
+                frame += symbolInput;
+            }
+
+            Console.Write(frame);
+            Console.Write("\n" + middleLine + "\n");
+            Console.Write(frame);
+        }
+    }
+}
